cf6759f23ea8b9a09ef4d8af27bf23f054aace75
Seekaplayer <admin@seekaplayer.com>
2023-09-17 11:11:09 -0400

    Made the header a component

---
+++ components/Header.vue
@@ -0,0 +1,44 @@
+<template>
+  <header class="py-5 md:py-2 shadow sticky top-0 mb-5 md:mb-10 z-10">
+    <nav
+      class="flex flex-col md:flex-row justify-between items-center container mx-auto px-5 md:px-0"
+    >
+      <NuxtLink to="/"
+        ><Nuxt-img class="logo" src="/images/logo.png" alt="Seekaplayer Logo"
+      /></NuxtLink>
+      <ul class="flex gap-5 mt-5 md:mt-0">
+        <li>
+          <NuxtLink to="/">Home</NuxtLink>
+        </li>
+        <li><NuxtLink to="/about">About</NuxtLink></li>
+        <li><NuxtLink to="/partners">Partners</NuxtLink></li>
+        <li>
+          <NuxtLink to="https://linktr.ee/seekaplayer" target="_blank"
+            >Socials</NuxtLink
+          >
+        </li>
+        <li><NuxtLink to="/contact">Contact</NuxtLink></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script setup></script>
+
+<style scoped>
+  header {
+    background: #1c1917;
+  }
+  .logo {
+    height: 75px;
+  }
+  nav ul li a {
+    color: #fff;
+    font-size: 1.125rem;
+  }
+  nav ul li a:hover {
+    color: #f8fafc;
+    transition: all 0.3s;
+    border-bottom: 1px solid rgba(248, 250, 252, 1);
+  }
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,26 +1,5 @@
 <template>
-  <header class="py-5 md:py-2 shadow sticky top-0 mb-5 md:mb-10 z-10">
-    <nav
-      class="flex flex-col md:flex-row justify-between items-center container mx-auto px-5 md:px-0"
-    >
-      <NuxtLink to="/"
-        ><Nuxt-img class="logo" src="/images/logo.png" alt="Seekaplayer Logo"
-      /></NuxtLink>
-      <ul class="flex gap-5 mt-5 md:mt-0">
-        <li>
-          <NuxtLink to="/">Home</NuxtLink>
-        </li>
-        <li><NuxtLink to="/about">About</NuxtLink></li>
-        <li><NuxtLink to="/partners">Partners</NuxtLink></li>
-        <li>
-          <NuxtLink to="https://linktr.ee/seekaplayer" target="_blank"
-            >Socials</NuxtLink
-          >
-        </li>
-        <li><NuxtLink to="/contact">Contact</NuxtLink></li>
-      </ul>
-    </nav>
-  </header>
+  <Header />
   <div>
     <slot />
   </div>
@@ -33,19 +12,4 @@
   const year = new Date().getFullYear();
 </script>
 
-<style scoped>
-  header {
-    background: #1c1917;
-  }
-  .logo {
-    height: 75px;
-  }
-  nav ul li a {
-    color: #fff;
-    font-size: 1.125rem;
-  }
-  nav ul li a:hover {
-    color: #a855f7;
-    transition: all 0.3s;
-  }
-</style>
+<style scoped></style>
