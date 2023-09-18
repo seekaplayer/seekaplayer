fe1c873be2b38457a626ce3b4608f6508ce8a7e9
Seekaplayer <admin@seekaplayer.com>
2023-09-18 09:20:38 -0400

    started working on the homepage.

---
+++ layouts/homepage.vue
@@ -0,0 +1,27 @@
+<template>
+  <HomeHeader />
+  <div>
+    <slot />
+  </div>
+  <Footer />
+</template>
+
+<script setup></script>
+
+<style scoped>
+  header {
+    background: #1c1917;
+  }
+  .logo {
+    height: 75px;
+  }
+  nav ul li a {
+    color: #fff;
+    font-size: 1.125rem;
+  }
+  nav ul li a:hover {
+    color: #f8fafc;
+    transition: all 0.3s;
+    border-bottom: 1px solid rgba(248, 250, 252, 1);
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,34 @@
 <template>
-  <div></div>
+  <header class="hero">
+    <div class="flex flex-col px-10 container mx-auto">
+      <h1 class="text-white">Hello, My name is Seekaplayer</h1>
+      <p>Welcome to my website, enjoy your stay here!</p>
+      <div class="flex gap-5 flex-col md:flex-row justify-center">
+        <NuxLink class="btn w-full text-center" to="/about">About Me</NuxLink>
+        <NuxtLink
+          class="btn w-full text-center"
+          to="https://linktr.ee/seekaplayer"
+          target="_blank"
+          >Connect with Me</NuxtLink
+        >
+      </div>
+    </div>
+  </header>
 </template>
 
-<script setup></script>
+<script setup>
+  definePageMeta({
+    layout: "homepage",
+  });
+</script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+  .hero {
+    background: url("/images/hero.jpg") no-repeat center center;
+    background-attachment: fixed;
+    background-size: cover;
+    color: #fff;
+    max-width: 100%;
+    padding: 10em 0;
+  }
+</style>
