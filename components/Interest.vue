44cacfe81c832de32932a82f20b0defd1b72fcbd
Seekaplayer <admin@seekaplayer.com>
2023-09-18 07:50:44 -0400

    Made a component out of interest.

---
+++ components/Interest.vue
@@ -0,0 +1,122 @@
+<template>
+  <article class="mt-10">
+    <header>
+      <h2>Hi, I'm Seekaplayer</h2>
+    </header>
+    <p class="text-2xl leading-relaxed font-thin m-0">
+      Hello, my name is Seekaplayer, I'm a Web Designer/Developer and Content
+      creator. I love learning how to code and I enjoy creating content. I have
+      many more interested then just technology. I also really enjoy playing
+      video games as well and when I'm not doing either of those things I enjoy
+      spending time at the gym.
+    </p>
+  </article>
+  <article class="mt-10">
+    <div class="grid grid-cols-1 lg:grid-cols-2 gap-5 items-center">
+      <NuxtImg class="w-full" src="/images/streamer.jpg" alt="Streamer" />
+      <div>
+        <header>
+          <h2>Streaming/Content Creation</h2>
+          <hr class="mt-2 mb-5" />
+        </header>
+        <p class="text-2xl leading-relaxed font-thin m-0">
+          I've been streaming since around 2011/2012. I started out streaming on
+          YouTube Gaming and then transitioned to Twitch/Kick. Over the past few
+          years I've focused on streaming Gaming, Coding/Development and every
+          so often I'll do a Music or Fitness stream. I really love interacting
+          with my viewers and I've learned a lot from them.
+        </p>
+      </div>
+    </div>
+  </article>
+  <article class="mt-10">
+    <div class="grid grid-cols-1 lg:grid-cols-2 gap-5 items-center flex">
+      <div>
+        <header>
+          <h2>Coding/Technology</h2>
+          <hr class="mt-2 mb-5" />
+        </header>
+        <p class="text-2xl leading-relaxed font-thin m-0">
+          Hello, my name is Seekaplayer, I'm a Web Designer/Developer and
+          Content creator. I love learning how to code and I enjoy creating
+          content. I have many more interested then just technology. I also
+          really enjoy playing video games as well and when I'm not doing either
+          of those things I enjoy spending time at the gym.
+        </p>
+      </div>
+      <NuxtImg
+        class="w-full order-first lg:order-last"
+        src="/images/coding.jpg"
+        alt="Streamer"
+      />
+    </div>
+  </article>
+  <article class="mt-10">
+    <div class="grid grid-cols-1 lg:grid-cols-2 gap-5 items-center">
+      <NuxtImg class="w-full" src="/images/fitness.jpg" alt="Streamer" />
+      <div>
+        <header>
+          <h2>Health & Fitness</h2>
+          <hr class="mt-2 mb-5" />
+        </header>
+        <p class="text-2xl leading-relaxed font-thin m-0">
+          Health & Fitness are very important to me. Back in 2018 I was at my
+          most unhealthiest weighing at around 244.9 pounds at 5'2 1/2 which was
+          very unhealthy! I suffered from a lot of health problems because of
+          this and decided one day to make a life change. I went on a fitness
+          journey and lost a little over 131.1 pounds! This has forever changed
+          my life and when I'm not working or making content I work as a Group
+          Fitness Instructor to help others with their fitness goals.
+        </p>
+      </div>
+    </div>
+  </article>
+  <article class="mt-10">
+    <div class="grid grid-cols-1 lg:grid-cols-2 gap-5 items-center flex">
+      <div>
+        <header>
+          <h2>Gaming</h2>
+          <hr class="mt-2 mb-5" />
+        </header>
+        <p class="text-2xl leading-relaxed font-thin m-0">
+          I've been gaming since I was a kid. I started off on the Nintendo NES,
+          Super Nintendo, Sega Genesis, Nintendo 64, Playstation 1/2 and then
+          from there I got into PC Gaming. When I turned 18 I got my first big
+          Gaming PC which was a Gateway which had a Pentium 4 processor, 1 GB of
+          Ram, Windows XP and I installed an ATX 256 MB GPU in it. Over the
+          years I've had a keen interest in both Gaming PC's and Consoles. I've
+          owned mostly XBOX and I've had a few Playstation consoles as well. As
+          of now I primarily game on a PC.
+        </p>
+      </div>
+      <NuxtImg
+        class="w-full order-first lg:order-last"
+        src="/images/gaming.jpg"
+        alt="Streamer"
+      />
+    </div>
+  </article>
+  <article class="mt-10">
+    <div class="grid grid-cols-1 lg:grid-cols-2 gap-5 items-center">
+      <NuxtImg class="w-full" src="/images/guitar.jpg" alt="Streamer" />
+      <div>
+        <header>
+          <h2>Music</h2>
+          <hr class="mt-2 mb-5" />
+        </header>
+        <p class="text-2xl leading-relaxed font-thin m-0">
+          Music has always been a big part of my life. I've been playing guitar
+          since I was a kid and I've dabbled in other instruments as well. My
+          first instrument was the guitar but I've also have played some on the
+          piano, drums, bass, saxophone, trumpet, and others. I love all types
+          of music from classical to heavy metal. I enjoy listening to music and
+          jamming along with songs I really love.
+        </p>
+      </div>
+    </div>
+  </article>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
